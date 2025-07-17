ca55114c2e9d5b9947bd52050eae8d02676b5c39
desmonddam <markettollls7@gmail.com>
2025-07-17 17:50:23 -0400

    added env files

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// const csslogger = require('vite-postcss-tools');
-
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    target: "es2015", // Ensures compatibility with older browsers
+    target: "es2015",
   },
 });
-
-// module.exports = defineConfig({
-//   plugins: [react(), csslogger()],
-// });
